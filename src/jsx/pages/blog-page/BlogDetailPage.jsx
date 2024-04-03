9069187affb427711f43967919960f59abed0626
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-04-03 15:46:45 +0330

    completed single blog page

--- src/jsx/pages/blog-page/BlogDetailPage.jsx
+++ src/jsx/pages/blog-page/BlogDetailPage.jsx
@@ -3,6 +3,7 @@ import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
 import { useLocation } from 'react-router-dom';
 import '../../../css/pages/blogs-page/BlogsPageStyle.css';
+import parse from 'html-react-parser';
 
 export default function BlogDetailPage() {
   const location = useLocation();
@@ -15,8 +16,8 @@ export default function BlogDetailPage() {
     isError
   } = useQuery({
     queryFn: async () => {
-      const {data} = await axios.get('https://utsmm.liara.run/api/blogs');
-      return data.entities.blogs.find(item => item.slug === lastRoute)
+      const {data} = await axios.get(`https://utsmm.liara.run/api/blogs/${lastRoute}`);
+      return data.entities.blog;
     }
   });
 
@@ -30,9 +31,22 @@ export default function BlogDetailPage() {
                     ? <h1>There was an error</h1>
                     : (
                         <>
-                          <img className="poster" src={data.image} alt={data.short_description}/>
-                          <h1 className={'title'}>{data.title}</h1>
-                          <p className={'content'}>{data.short_description}</p>
+                          <div className={'info-holder'}>
+                            {
+                                data.keywords.length !== 0 && <div className={'keywords-holder'}>
+                                  {
+                                    data.keywords.map((item, index) => (
+                                        <div className={'keyword'} key={index}>#{item}</div>
+                                    ))
+                                  }
+                                </div>
+                            }
+                            <img className="poster" src={`https://utsmm.liara.run/${data.image}`}
+                                 alt={data.short_description}/>
+                            <h1 className={'title'}>{data.title}</h1>
+                            <p className={'content'}>{data.short_description}</p>
+                          </div>
+                          {parse(data.content)}
                         </>
                     )
           }
