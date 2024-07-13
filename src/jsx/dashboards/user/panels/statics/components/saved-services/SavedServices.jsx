8548b07576931f18215605293d710f700e9b322b
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-07-13 09:15:22 +0330

    added simple statment to show a text when there is no favorite items

--- src/jsx/dashboards/user/panels/statics/components/saved-services/SavedServices.jsx
+++ src/jsx/dashboards/user/panels/statics/components/saved-services/SavedServices.jsx
@@ -24,90 +24,92 @@ export default function SavedServices({data}) {
             <div className="body">
 
                 {
-                    data.map((item, index) => {
-                        return <div
-                            key={index}
-                            className="item">
-                            <div className="item-header">
-                                <MaxLineText
-                                    maxLine={2}
-                                    content={
-                                        <h1>
-                                            {item.title}
-                                        </h1>
-                                    } />
-                            </div>
-                            <div className="item-body">
-                                <ul className="properties">
-                                    <li>
-                                        <div className="label">
-                                            <Icon icon="eos-icons:service" />
-                                            <span>
+                    (data.length === 0)
+                        ? <h1>There is nothing to show</h1>
+                        : data.map((item, index) => {
+                            return <div
+                                key={index}
+                                className="item">
+                                <div className="item-header">
+                                    <MaxLineText
+                                        maxLine={2}
+                                        content={
+                                            <h1>
+                                                {item.title}
+                                            </h1>
+                                        } />
+                                </div>
+                                <div className="item-body">
+                                    <ul className="properties">
+                                        <li>
+                                            <div className="label">
+                                                <Icon icon="eos-icons:service" />
+                                                <span>
                                                 ID
                                             </span>
-                                        </div>
-                                        <div className="value">
-                                            {item.id}
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="label">
-                                            <Icon icon="tabler:math-min" />
-                                            <span>
+                                            </div>
+                                            <div className="value">
+                                                {item.id}
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="label">
+                                                <Icon icon="tabler:math-min" />
+                                                <span>
                                                 Min Order
                                             </span>
-                                        </div>
-                                        <div className="value">
-                                            {item.min}
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="label">
-                                            <Icon icon="tabler:math-max" />
-                                            <span>
+                                            </div>
+                                            <div className="value">
+                                                {item.min}
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="label">
+                                                <Icon icon="tabler:math-max" />
+                                                <span>
                                                 Max Order
                                             </span>
-                                        </div>
-                                        <div className="value">
-                                            {item.max}
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="label">
-                                            <Icon icon="mdi:wiper" />
-                                            <span>
+                                            </div>
+                                            <div className="value">
+                                                {item.max}
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="label">
+                                                <Icon icon="mdi:wiper" />
+                                                <span>
                                                 Per 1k
                                             </span>
-                                        </div>
-                                        <div className="value">
-                                            ${item.rate}
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="label">
-                                            <Icon icon="ic:baseline-auto-fix-normal" />
-                                            <span>
+                                            </div>
+                                            <div className="value">
+                                                ${item.rate}
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="label">
+                                                <Icon icon="ic:baseline-auto-fix-normal" />
+                                                <span>
                                                 Avarage Time
                                             </span>
-                                        </div>
-                                        <div className="value">
-                                            {"N/A"}
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="item-buttons">
-                                <Link to={`/user/dashboard/New-Order?service=${item.id}&category=${item.category_id}`}>
-                                    <button className="buy">
+                                            </div>
+                                            <div className="value">
+                                                {"N/A"}
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="item-buttons">
+                                    <Link to={`/user/dashboard/New-Order?service=${item.id}&category=${item.category_id}`}>
+                                        <button className="buy">
                                         <span>
                                             Order
                                         </span>
-                                        <Icon icon="el:ok"/>
-                                    </button>
-                                </Link>
+                                            <Icon icon="el:ok"/>
+                                        </button>
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                    })
+                        })
                 }
 
 
