26c758576d9cebe2df722b08176cb4c75031be07
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-07-20 08:37:35 +0330

    added popular services api in /services

--- src/jsx/pages/services-page/component/PopularServices.jsx
+++ src/jsx/pages/services-page/component/PopularServices.jsx
@@ -6,179 +6,78 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Icon } from "@iconify/react";
 import { useState } from 'react';
 import { useFetch } from '../../../../lib/useFetch';
-import { API, SERVER } from '../../../../lib/envAccess';
+import BE_URL, { API, SERVER } from '../../../../lib/envAccess';
 import {Link} from "react-router-dom";
 
 
 export default function PopularServices() {
+    // Fetching the data
+    const [data, error, loading] = useFetch(`${BE_URL}/popular-services`);
+
+    // Returning JSX
     return (
         <section className="suggested-services">
-            <Swiper
-                modules={[Navigation, Pagination, A11y]}
-                spaceBetween={20}
-                slidesPerView={4}
-                navigation={{
-                    nextEl: ".arrow-right",
-                    prevEl: ".arrow-left",
-                    disabledClass: "arrow-disabled"
-                }}
-                pagination={
-                    {
-                        clickable: true,
-                        el: ".swiper-custom-pagination"
-                    }
-                }
+            {
+                (loading)
+                    ? <h1>Loading</h1>
+                    : (error)
+                        ? <h1>There was an error while fetching the data</h1>
+                        : (
+                            <Swiper
+                                modules={[Navigation, Pagination, A11y]}
+                                spaceBetween={20}
+                                slidesPerView={4}
+                                navigation={{
+                                    nextEl: ".arrow-right",
+                                    prevEl: ".arrow-left",
+                                    disabledClass: "arrow-disabled"
+                                }}
+                                pagination={{clickable: true, el: ".swiper-custom-pagination"}}
+                                breakpoints={{
+                                    0: {slidesPerView: 1.25,},
+                                    400: {slidesPerView: 2,},
+                                    600: {slidesPerView: 2.5,},
+                                    800: {slidesPerView: 3},
+                                    1000: {slidesPerView: 3.5},
+                                    1200: {slidesPerView: 4},
+                                    1400: {slidesPerView: 5},
+                                    1600: {slidesPerView: 5.5},
+                                    1800: {slidesPerView: 6},
+                                    1900: {slidesPerView: 6.5}
+                                }}
 
-
-                breakpoints={{
-                    0: {
-                        slidesPerView: 1.25,
-                    },
-                    400: {
-                        slidesPerView: 2,
-                    },
-                    600: {
-                        slidesPerView: 2.5,
-                    },
-                    800: {
-                        slidesPerView: 3
-                    },
-                    1000: {
-                        slidesPerView: 3.5
-                    },
-                    1200: {
-                        slidesPerView: 4
-                    },
-                    1400: {
-                        slidesPerView: 5
-                    },
-                    1600: {
-                        slidesPerView: 5.5
-                    },
-                    1800: {
-                        slidesPerView: 6
-                    },
-                    1900: {
-                        slidesPerView: 6.5
-                    }
-                }}
-
-            >
-                <SwiperSlide className='first-slide slide'>
-                    <Link to={'#'} className="card">
-                        <div className="header">
-                            <img
-                                src={require("../../../../images/services-page/services/suggestions-offers/2.png")} />
-                        </div>
-                        <div className="body">
-                            <h1>POPULAR SERVICES</h1>
-                            <small>
-                                New Upcoming Services , Ready For Boosting Your Account
-                            </small>
-                        </div>
-                        <div className="button">
-                            <button>
-                                See all
-                            </button>
-                        </div>
-                    </Link>
-                </SwiperSlide>
-                <SwiperSlide className='first-slide slide'>
-                    <Link to={'#'} className="card">
-                        <div className="header">
-                            <img
-                                src={require("../../../../images/services-page/services/suggestions-offers/2.png")} />
-                        </div>
-                        <div className="body">
-                            <h1>POPULAR SERVICES</h1>
-                            <small>
-                                New Upcoming Services , Ready For Boosting Your Account
-                            </small>
-                        </div>
-                        <div className="button">
-                            <button>
-                                See all
-                            </button>
-                        </div>
-                    </Link>
-                </SwiperSlide>
-                <SwiperSlide className='first-slide slide'>
-                    <Link to={'#'} className="card">
-                        <div className="header">
-                            <img
-                                src={require("../../../../images/services-page/services/suggestions-offers/2.png")} />
-                        </div>
-                        <div className="body">
-                            <h1>POPULAR SERVICES</h1>
-                            <small>
-                                New Upcoming Services , Ready For Boosting Your Account
-                            </small>
-                        </div>
-                        <div className="button">
-                            <button>
-                                See all
-                            </button>
-                        </div>
-                    </Link>
-                </SwiperSlide>
-                <SwiperSlide className='first-slide slide'>
-                    <Link to={'#'} className="card">
-                        <div className="header">
-                            <img
-                                src={require("../../../../images/services-page/services/suggestions-offers/2.png")} />
-                        </div>
-                        <div className="body">
-                            <h1>POPULAR SERVICES</h1>
-                            <small>
-                                New Upcoming Services , Ready For Boosting Your Account
-                            </small>
-                        </div>
-                        <div className="button">
-                            <button>
-                                See all
-                            </button>
-                        </div>
-                    </Link>
-                </SwiperSlide>
-                <SwiperSlide className='first-slide slide'>
-                    <Link to={'#'} className="card">
-                        <div className="header">
-                            <img
-                                src={require("../../../../images/services-page/services/suggestions-offers/2.png")} />
-                        </div>
-                        <div className="body">
-                            <h1>POPULAR SERVICES</h1>
-                            <small>
-                                New Upcoming Services , Ready For Boosting Your Account
-                            </small>
-                        </div>
-                        <div className="button">
-                            <button>
-                                See all
-                            </button>
-                        </div>
-                    </Link>
-                </SwiperSlide>
-                <SwiperSlide className='first-slide slide'>
-                    <Link to={'#'} className="card">
-                        <div className="header">
-                            <img
-                                src={require("../../../../images/services-page/services/suggestions-offers/2.png")} />
-                        </div>
-                        <div className="body">
-                            <h1>POPULAR SERVICES</h1>
-                            <small>
-                                New Upcoming Services , Ready For Boosting Your Account
-                            </small>
-                        </div>
-                        <div className="button">
-                            <button>
-                                See all
-                            </button>
-                        </div>
-                    </Link>
-                </SwiperSlide>
-            </Swiper>
+                            >
+                                {
+                                    Object.values(data?.entities)[0].map((item, index) => (
+                                        <SwiperSlide className='first-slide slide' key={index}>
+                                            <Link to={`/user/dashboard/New-Order?service=${item.id}&category=${item.category_id}`}
+                                                  className="card">
+                                                <div className="header">
+                                                    <img
+                                                        src={require("../../../../images/services-page/services/suggestions-offers/2.png")}/>
+                                                </div>
+                                                <div className="body">
+                                                    <h1 style={{
+                                                        overflow: 'hidden',
+                                                        textOverflow: 'ellipsis',
+                                                        whiteSpace: 'nowrap'
+                                                    }}>{item.title}</h1>
+                                                    <small>
+                                                        New Upcoming Services , Ready For Boosting Your Account
+                                                    </small>
+                                                </div>
+                                                <div className="button">
+                                                    <button>
+                                                        See
+                                                    </button>
+                                                </div>
+                                            </Link>
+                                        </SwiperSlide>
+                                    ))
+                                }
+                            </Swiper>
+                        )
+            }
         </section>
     )
 }
