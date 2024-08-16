21f7e768ff89c2d2350f4cfd750d10cbe22759b0
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-08-16 16:55:51 +0330

    fixed bugs of tickets panel in user dashboard

--- src/jsx/dashboards/user/panels/tickets/componetns/TicketHistory.jsx
+++ src/jsx/dashboards/user/panels/tickets/componetns/TicketHistory.jsx
@@ -1,7 +1,7 @@
 import { Icon } from "@iconify/react"
 import { useFetch } from "../../../../../../lib/useFetch";
 import BE_URL, { API } from "../../../../../../lib/envAccess";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Modal from "../../../../../pop-ups/modal";
 import Swal from "sweetalert2";
 import HandleFetchError from "../../../../../../lib/handleFetchError";
@@ -27,10 +27,15 @@ const TicketHistory = () => {
         const [data, setData] = useState({});
         const [fetchLoading, setFetchLoading] = useState(true);
         const [fetchError, setFetchError] = useState('');
+        const [count, setCount] = useState(0);
+
+        // Defining ref
+        const fileInputRef = useRef(null);
 
         // Using useEffect hook to fetch data of ticket
         useEffect(() => {
             if (modalOpened) {
+                setFetchLoading(true);
                 fetch(`${BE_URL}/tickets/${id}`, {
                     method: "GET",
                     headers: {
@@ -59,7 +64,27 @@ const TicketHistory = () => {
                         setFetchError('There was an error while fetching the data')
                     })
             }
-        }, [modalOpened]);
+        }, [modalOpened, count]);
+
+        // Defining states of new message
+        const [imageFile, setImageFile] = useState();
+        const [message, setMessage] = useState();
+
+        // Defining a function to handle on change of file input
+        const handleFileChange = (e) => {
+            const file = e.target.files[0]
+
+            const fileReader = new FileReader()
+            fileReader.onload = (e) => {
+                const result = e.target.result
+            }
+
+            fileReader.readAsDataURL(file);
+            setImageFile(e.target.files[0]);
+        }
+
+        // Defining a function to add to count
+        const refetch = () => setCount(prev => prev + 1);
 
         // Returning JSX
         return (
@@ -71,7 +96,7 @@ const TicketHistory = () => {
                             : (fetchError)
                                 ? <h1>{fetchError}</h1>
                                 : (
-                                    <div className={'table_2'}>
+                                    <div className={'table_2'} style={{marginBottom: '200px'}}>
                                         <div className={'col'}>ID</div>
                                         <div className={'col'}>{data.id}</div>
                                         <div className={'col'}>Subject</div>
@@ -91,15 +116,19 @@ const TicketHistory = () => {
                                                                 data.messages.map((item, index) => (
                                                                     <div key={index} className={'message'}>
                                                                         <div className={'message-content-holder'}>
-                                                                            <div className={`message-content ${(item.is_answer) ? 'admin' : 'user'}`}>
+                                                                            <div className={`message-content ${(item.is_answer === "1") ? 'admin' : 'user'}`}>
                                                                                 <p className={'message-text'}>{item.content}</p>
                                                                                 <span className={'message-date'}>{new Date(item.created_at).toLocaleDateString()}</span>
                                                                             </div>
                                                                             {
                                                                                 (item.attachment)
                                                                                     ? (
-                                                                                        <div className={`attachment-holder ${(item.is_answer) ? 'admin' : 'user'}`}>
-                                                                                            <div className={'attachment'} style={{background: `url(${item.attachment.attachment_url})`}}>
+                                                                                        <div className={`attachment-holder ${(item.is_answer === "1") ? 'admin' : 'user'}`}>
+                                                                                            <div className={'attachment'}>
+                                                                                                <img
+                                                                                                    src={item.attachment.attachment_url}
+                                                                                                    alt={item.attachment.attachment_name}
+                                                                                                />
                                                                                                 <div className={'attachment-name'}>{item.attachment.attachment_name}</div>
                                                                                             </div>
                                                                                         </div>
@@ -116,6 +145,63 @@ const TicketHistory = () => {
                                     </div>
                                 )
                     }
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+
+                        const myHeaders = new Headers();
+                        myHeaders.append("Accept", "application/json");
+                        myHeaders.append("X-Requested-With", "XMLHttpRequest");
+                        myHeaders.append("Authorization", `Bearer ${JSON.parse(localStorage.getItem('token'))}`);
+
+                        const formdata = new FormData();
+                        formdata.append("content", message);
+                        formdata.append("attachment", imageFile, "[PROXY]");
+
+                        const requestOptions = {
+                            method: "POST",
+                            headers: myHeaders,
+                            body: formdata,
+                            redirect: "follow"
+                        };
+
+                        fetch(`${BE_URL}/tickets/${id}/messages`, requestOptions)
+                            .then((response) => response.json())
+                            .then((result) => {
+                                HandleFetchError({
+                                    data: result,
+                                    lineBreak: false,
+                                    callbackSuccess: (message) => {
+                                        Swal.fire({icon: 'success', text: message})
+                                        refetch();
+                                    },
+                                    callbackError: (message) => Swal.fire({icon: 'error', text: message})
+                                })
+                            })
+                            .catch(() => {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'There was an error while fetching the data'
+                                })
+                            })
+                    }} className={'send-message-holder'}>
+                        <div className={'message'}>
+                            <input onChange={(e) => setMessage(e.target.value)} placeholder={'Type Something here'} type="text" className={'input'} required                                 minLength={10} maxLength={1500}/>
+                            <button onClick={() => fileInputRef.current.click()} type={'button'} className={'attachment-button'}>
+                                <Icon icon="gridicons:attachment" width={20} height={20}/>
+                            </button>
+                            <input
+                                required
+                                type="file"
+                                accept="image/*"
+                                ref={fileInputRef}
+                                style={{display: 'none'}}
+                                onChange={handleFileChange}
+                            />
+                        </div>
+                        <button className={'send-btn'} type={'submit'}>
+                            <Icon icon="material-symbols:send" width={20} height={20}/>
+                        </button>
+                    </form>
                 </Modal>
                 <div className="item-header">
                     <div className="status">
