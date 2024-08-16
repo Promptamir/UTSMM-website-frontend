bb75dceb986bc36f23974c13cd7abcce01a99708
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-08-16 17:10:14 +0330

    added free credits

--- src/jsx/dashboards/user/panels/free-credits/FreeCredits.jsx
+++ src/jsx/dashboards/user/panels/free-credits/FreeCredits.jsx
@@ -1,6 +1,15 @@
+// Animation
+import EmailVerify from "./components/EmailVerify"
+import TrustPilot from "./components/TrustPilot"
+import ShareTweet from "./components/ShareTweet"
+
 const FreeCredits = () => {
     return (
-        <main className="panel-free-creidits" />
+        <main className="panel-free-creidits">
+            <EmailVerify />
+            <TrustPilot />
+            <ShareTweet />
+        </main>
     )
 }
 
